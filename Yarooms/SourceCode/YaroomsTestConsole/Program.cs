d6ed4827f6d1c149fa42fef5b04fa64109e71977
Patrick Theobald <theobald@theobald.local>
2020-09-04 15:40:27 +0200

    Initial commit for source code

---
+++ Yarooms/SourceCode/YaroomsTestConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json.Linq;
+using PeakboardExtensionYarooms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YaroomsTestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Docu: https://www.yarooms.com/help/api/#get-all-meetings
+
+            List<YaroomsMeeting> mymeetings = YaroomsHelper.GetAllMeetings("", "", "");
+                
+            Console.ReadLine();
+
+        }
+
+    }
+}
