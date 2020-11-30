19904928bc6b47e59840ef58403e90facb806edd
Thilo Brosinsky <brosinsky@theobald.local>
2020-11-30 08:59:32 +0100

    Added Fritz Smart Home Extension

---
+++ FritzSmartHome/SourceCode/ConsoleTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using AVMFritz;
+using System.Globalization;
+using System.Linq;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fh = new FritzHelper();
+            var thermostats = fh.GetThermostats("fritz.box", string.Empty, "XXXXXX");
+            fh.SetThermostatTemperature("fritz.box", string.Empty, "XXXXXX", thermostats.First(t => t.Name == "Büro"), 20.5);
+        }
+    }
+}
