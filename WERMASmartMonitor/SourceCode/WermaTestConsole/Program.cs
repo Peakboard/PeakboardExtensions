ed0538c3cbb8813c3a257dbbb7458cc0a0e9a250
Benjamin Gallier <bgallier@unistra.fr>
2021-05-06 17:36:47 +0200

    New Peakboard Extension added : WERMASmartMonitor

---
+++ WERMASmartMonitor/SourceCode/WermaTestConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace WermaTestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        
+    }
+    
+}
