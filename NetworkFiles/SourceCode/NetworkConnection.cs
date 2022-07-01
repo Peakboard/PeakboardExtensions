6ec333b5d653de5730f10034fe1faea81259497f
Thilo Brosinsky <brosinsky@theobald.local>
2022-07-01 15:54:47 +0200

    Added network files extension

---
+++ NetworkFiles/SourceCode/NetworkConnection.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.ComponentModel;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+namespace PeakboardExtensionNetworkFiles
+{
+    public class NetworkConnection : IDisposable
+    {
+        public string NetworkName { get; }
+
+        public NetworkConnection(string networkName, NetworkCredential credentials)
+        {
+            var originalNetworkName = networkName;
+
+            // Try it first with the ip of the dns
+            // Here we get a error if the designer (windows desktop) user is an other user as the credentials
+            // this does not happen if we change the dns to ip
+            for (var i = 0; i < 2; i++)
+            {
+                // first try it with the ip
+                if (i == 0)
+                {
+                    var rgx = new Regex(@"^\\\\(.*?)\\");
+                    var dns = rgx.Match(networkName).Value;
+                    if (!string.IsNullOrEmpty(dns))
+                    {
+                        dns = dns.Trim('\\');
+                        var ip = Dns.GetHostAddresses(dns).Length > 0 ? Dns.GetHostAddresses(dns)[0].ToString() : null;
+
+                        if (!string.IsNullOrEmpty(ip))
+                        {
+                            networkName = Regex.Replace(networkName, @"^\\\\.*?\\", $@"\\{ip}\");
+                        }
+                    }
+                }
+                // if it did now worked try it with the dns (which mostly will fail then as well)
+                else
+                {
+                    networkName = originalNetworkName;
+                }
+
+                NetworkName = networkName;
+
+                var netResource = new NetResource { Scope = ResourceScope.GlobalNetwork, ResourceType = ResourceType.Disk, DisplayType = ResourceDisplaytype.Share, RemoteName = networkName };
+
+                var userName = string.IsNullOrEmpty(credentials.Domain)
+                    ? credentials.UserName
+                    : $@"{credentials.Domain}\{credentials.UserName}";
+
+                var result = WNetAddConnection2(
+                    netResource,
+                    credentials.Password,
+                    userName,
+                    0);
+
+                // try it with the original name
+                if (result != 0 && i == 0 && !networkName.Equals(originalNetworkName, StringComparison.InvariantCulture))
+                {
+                    continue;
+                }
+
+                if (result != 0)
+                {
+                    throw new Win32Exception(result);
+                }
+
+                return;
+            }
+        }
+
+        ~NetworkConnection()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            _ = WNetCancelConnection2(NetworkName, 0, true);
+        }
+
+#pragma warning disable CA2101 // This is not working with CharSet Unicode
+        [DllImport("mpr.dll", CharSet = CharSet.Ansi)]
+        private static extern int WNetAddConnection2(NetResource netResource, string password, string username, int flags);
+
+        [DllImport("mpr.dll", CharSet = CharSet.Ansi)]
+        private static extern int WNetCancelConnection2(string name, int flags, bool force);
+#pragma warning restore CA2101  // This is not working with CharSet Unicode
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public class NetResource
+    {
+        public ResourceScope Scope;
+        public ResourceType ResourceType;
+        public ResourceDisplaytype DisplayType;
+        public int Usage;
+        public string LocalName;
+        public string RemoteName;
+        public string Comment;
+        public string Provider;
+    }
+
+    public enum ResourceScope
+    {
+        Connected = 1,
+        GlobalNetwork,
+        Remembered,
+        Recent,
+        Context
+    }
+
+    public enum ResourceType
+    {
+        Any = 0,
+        Disk = 1,
+        Print = 2,
+        Reserved = 8
+    }
+
+    public enum ResourceDisplaytype
+    {
+        Generic = 0x0,
+        Domain = 0x01,
+        Server = 0x02,
+        Share = 0x03,
+        File = 0x04,
+        Group = 0x05,
+        Network = 0x06,
+        Root = 0x07,
+        Shareadmin = 0x08,
+        Directory = 0x09,
+        Tree = 0x0a,
+        Ndscontainer = 0x0b
+    }
+}
---
+++ NetworkFiles/SourceCode/NetworkFilesDataSource.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Peakboard.ExtensionKit;
+using PeakboardExtensionNetworkFiles;
+
+namespace PeakboardExtensionNetworkFiles
+{
+    [ExtensionIcon("PeakboardExtensionNetworkFiles.File.png")]
+    public class CatFactsExtension : ExtensionBase
+    {
+        protected override ExtensionDefinition GetDefinitionOverride()
+        {
+            return new ExtensionDefinition
+            {
+                ID = "NetworkFiles",
+                Name = "Network files",
+                Version = "1.0",
+                Author = "Peakboard GmbH",
+                Company = "Peakboard GmbH",
+                Description = "Reads all files of an unc path folder"
+            };
+        }
+
+        protected override CustomListCollection GetCustomListsOverride()
+        {
+            return new CustomListCollection
+            {
+                new NetworkFilesList(),
+            };
+        }
+    }
+}
