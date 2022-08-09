32221d096cc3fa3577004f1b179c7212c2865f3c
Thilo Brosinsky <brosinsky@theobald.local>
2022-08-09 10:57:01 +0200

    added demo for input and output parameters

---
+++ Samples/CalcDemo/SourceCode/CalcDemoCustomList.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Peakboard.ExtensionKit;
+
+namespace PeakboardExtensionCalcDemo
+{
+    [Serializable]
+    class CalcDemoCustomList : CustomListBase
+    {
+        protected override CustomListDefinition GetDefinitionOverride()
+        {
+            return new CustomListDefinition
+            {
+                ID = $"CalcDemoList",
+                Name = "Calculation demo",
+                PropertyInputPossible = false,
+                Functions =
+                {
+                    new CustomListFunctionDefinition()
+                    {
+                        Name = "calc",
+                        InputParameters = new CustomListFunctionInputParameterDefinitionCollection
+                        {
+                            new CustomListFunctionInputParameterDefinition
+                            {
+                                Name = "var a",
+                                Description = "var a desc",
+                                Optional = false,
+                                Type = CustomListFunctionParameterTypes.Number
+                            },
+                            new CustomListFunctionInputParameterDefinition
+                            {
+                                Name = "var b",
+                                Description = "var b desc",
+                                Optional = false,
+                                Type = CustomListFunctionParameterTypes.Number
+                            },
+                            new CustomListFunctionInputParameterDefinition
+                            {
+                                Name = "op",
+                                Description = "op desc",
+                                Optional = false,
+                                Type = CustomListFunctionParameterTypes.String
+                            }
+                        },
+                        ReturnParameters = new CustomListFunctionReturnParameterDefinitionCollection
+                        {
+                            new CustomListFunctionReturnParameterDefinition
+                            {
+                                Name = "result",
+                                Description = "result desc",
+                                Type = CustomListFunctionParameterTypes.Number
+                            }
+                        }
+                    }
+                }
+            };
+        }
+
+        protected override CustomListExecuteReturnContext ExecuteFunctionOverride(CustomListData data, CustomListExecuteParameterContext context)
+        {
+            var varA = double.Parse(context.Values[0].StringValue);
+            var varB = double.Parse(context.Values[1].StringValue);
+            var op = context.Values[2].StringValue;
+            var ret = new CustomListExecuteReturnContext();
+
+            switch (op)
+            {
+                case "+":
+                    ret.Add(varA + varB);
+                    break;
+                case "-":
+                    ret.Add(varA - varB);
+                    break;
+                case "*":
+                    ret.Add(varA * varB);
+                    break;
+                case "/":
+                    ret.Add(varA / varB);
+                    break;
+            }
+
+            return ret;
+        }
+
+        protected override CustomListColumnCollection GetColumnsOverride(CustomListData data)
+        {
+            return new CustomListColumnCollection
+            {
+                new CustomListColumn("Dummy", CustomListColumnTypes.Boolean)
+            };
+        }
+
+        protected override CustomListObjectElementCollection GetItemsOverride(CustomListData data)
+        {
+            var items = new CustomListObjectElementCollection();
+            items.Add(new CustomListObjectElement { { "Dummy", true}});
+            return items;
+        }
+    }
+}
---
+++ Samples/CalcDemo/SourceCode/CalcDemoDataSource.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Peakboard.ExtensionKit;
+
+
+namespace PeakboardExtensionCalcDemo
+{
+    public class CalcDemoExtension : ExtensionBase
+    {
+        protected override ExtensionDefinition GetDefinitionOverride()
+        {
+            return new ExtensionDefinition
+            {
+                ID = "CalcDemo",
+                Name = "Calculation demo",
+                Description = "This is a sample implementation for input and output parameters",
+                Version = "1.0",
+                Author = "Thilo Brosinsky",
+                Company = "Peakboard GmbH",
+                Copyright = "Peakboard GmbH",
+            };
+        }
+
+        protected override CustomListCollection GetCustomListsOverride()
+        {
+            return new CustomListCollection
+            {
+                new CalcDemoCustomList(),
+            };
+        }
+    }
+}
