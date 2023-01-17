6727a744593e3d5e022fbd3194f8cc0fe53f906b
Thilo Brosinsky <thilo.brosinsky@peakboard.com>
2023-01-17 11:23:37 +0100

    Added Sick extension

---
+++ SickExtension/SickExtension/SickExtension.cs
@@ -0,0 +1,35 @@
+﻿using Peakboard.ExtensionKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SickExtension
+{
+    [ExtensionIcon("Peakboard.Extensions.SickExtension.Sick.png")]
+    public class SickExtension : ExtensionBase
+    {
+        protected override ExtensionDefinition GetDefinitionOverride()
+        {
+            return new ExtensionDefinition
+            {
+                ID = "Peakboard.Extensions.SickExtension",
+                Name = "Sick",
+                Description = "This is an Extension for accessing a Sick sensor.",
+                Version = "1.0",
+                Author = "Peakboard Team",
+                Company = "Peakboard GmbH",
+                Copyright = "Copyright © 2022"
+            };
+        }
+
+        protected override CustomListCollection GetCustomListsOverride()
+        {
+            return new CustomListCollection
+            {
+                new SickListBase()
+            };
+        }
+    }
+}
