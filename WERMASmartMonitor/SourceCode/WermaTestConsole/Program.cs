314e1f096ffb238ad324ee4246ef8307d07d0d0c
Thilo Brosinsky <thilo.brosinsky@peakboard.com>
2023-02-24 09:02:32 +0100

    Fix for write to werma

--- WERMASmartMonitor/SourceCode/WermaTestConsole/Program.cs
+++ WERMASmartMonitor/SourceCode/WermaTestConsole/Program.cs
@@ -9,6 +9,16 @@ namespace WermaTestConsole
     {
         static void Main(string[] args)
         {
+            // WERMA want to write some temp data here. Seams they have a bug which does not recognize the folder is missing
+            if (!Directory.Exists(@"C:\ProgramData\WERMA"))
+            {
+                Directory.CreateDirectory(@"C:\ProgramData\WERMA");
+                if (!Directory.Exists(@"C:\ProgramData\WERMA\WERMA-WIN-3.0"))
+                {
+                    Directory.CreateDirectory(@"C:\ProgramData\WERMA\WERMA-WIN-3.0");
+                }
+            }
+
             var host = "192.168.20.70\\WERMAWIN";
             var command = $@"{AppDomain.CurrentDomain.BaseDirectory}\WermaUtilities\WIN-CLI.exe /server {host.Split('\\').First()} /switchcontrol ""macid:009E40"" 1 {args[0]}";
 
