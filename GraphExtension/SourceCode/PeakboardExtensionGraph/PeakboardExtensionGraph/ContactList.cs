39e50c01f462bd7cca9827351b7bd56e2aaa1a19
Havano-ano <yannishartmann@onlinehome.de>
2023-03-13 15:54:35 +0100

    Added CustomList
    Implemented some code to test msgraph authorization

---
+++ GraphExtension/SourceCode/PeakboardExtensionGraph/PeakboardExtensionGraph/ContactList.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace PeakboardExtensionGraph
+{
+
+    public class ContactList
+    {
+        public string odatacontext { get; set; }
+        public Contact[] value { get; set; }
+    }
+
+    public class Contact
+    {
+        public string odataetag { get; set; }
+        public string id { get; set; }
+        public DateTime createdDateTime { get; set; }
+        public DateTime lastModifiedDateTime { get; set; }
+        public string changeKey { get; set; }
+        public object[] categories { get; set; }
+        public string parentFolderId { get; set; }
+        public object birthday { get; set; }
+        public string fileAs { get; set; }
+        public string displayName { get; set; }
+        public string givenName { get; set; }
+        public string initials { get; set; }
+        public string middleName { get; set; }
+        public object nickName { get; set; }
+        public string surname { get; set; }
+        public string title { get; set; }
+        public object yomiGivenName { get; set; }
+        public object yomiSurname { get; set; }
+        public object yomiCompanyName { get; set; }
+        public string generation { get; set; }
+        public string[] imAddresses { get; set; }
+        public string jobTitle { get; set; }
+        public string companyName { get; set; }
+        public string department { get; set; }
+        public object officeLocation { get; set; }
+        public object profession { get; set; }
+        public object businessHomePage { get; set; }
+        public object assistantName { get; set; }
+        public object manager { get; set; }
+        public object[] homePhones { get; set; }
+        public object mobilePhone { get; set; }
+        public object[] businessPhones { get; set; }
+        public object spouseName { get; set; }
+        public string personalNotes { get; set; }
+        public object[] children { get; set; }
+        public Emailaddress[] emailAddresses { get; set; }
+        public Homeaddress homeAddress { get; set; }
+        public Businessaddress businessAddress { get; set; }
+        public Otheraddress otherAddress { get; set; }
+    }
+
+    public class Homeaddress
+    {
+    }
+
+    public class Businessaddress
+    {
+        public string countryOrRegion { get; set; }
+    }
+
+    public class Otheraddress
+    {
+    }
+
+    public class Emailaddress
+    {
+        public string name { get; set; }
+        public string address { get; set; }
+    }
+
+}
---
+++ GraphExtension/SourceCode/PeakboardExtensionGraph/PeakboardExtensionGraph/RequestBuilder.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace PeakboardExtensionGraph
+{
+    public class RequestBuilder
+    {
+        private string _accessToken;
+        private string path = "queries.json";
+    
+        private const string BaseUrl = "https://graph.microsoft.com/v1.0/me";
+        private Dictionary<string, string> _queries;
+
+        public RequestBuilder(string accessToken)
+        {
+            _accessToken = accessToken;
+            _queries = JsonConvert.DeserializeObject<Dictionary<string, string>>(readJson(path));
+        }
+
+        private string readJson(string p)
+        {
+            var streamReader = new StreamReader(p);
+            string value = streamReader.ReadToEnd();
+            streamReader.Close();
+            return value;
+        }
+
+        public HttpRequestMessage GetRequest(string key = null, RequestParameters parameters = null)
+        {
+            var suffix = "";
+            if(key != null)
+            {
+                _queries.TryGetValue(key, out suffix);
+            }
+
+            string url = BaseUrl + suffix;
+
+            string queryParams = "";
+
+            if(parameters != null)
+            {
+                queryParams = "?";
+                if (parameters.Filter != null)
+                {
+                    queryParams += $"$filter={parameters.Filter}";
+                }
+
+                if (parameters.OrderBy != null)
+                {
+                    if (queryParams != "?")
+                    {
+                        queryParams += "&";
+                    }
+
+                    queryParams += $"$orderBy={parameters.OrderBy}";
+                }
+
+                if (parameters.Skip != 0)
+                {
+                    if (queryParams != "?")
+                    {
+                        queryParams += "&";
+                    }
+
+                    queryParams += $"$skip={parameters.Skip}";
+                }
+
+                if (parameters.Top != 0)
+                {
+                    if (queryParams != "?")
+                    {
+                        queryParams += "&";
+                    }
+
+                    queryParams += $"top={parameters.Top}";
+                }
+
+                if (parameters.Select != null)
+                {
+                    if (queryParams != "?")
+                    {
+                        queryParams += "&";
+                    }
+
+                    queryParams += "$select=";
+                    foreach (var field in parameters.Select)
+                    {
+                        queryParams += $"{field},";
+                    }
+
+                    queryParams.Remove(queryParams.Length-1);
+                }
+            }
+            
+            
+            var request = new HttpRequestMessage
+            {
+                RequestUri = new Uri(url+queryParams),
+                Method = HttpMethod.Get
+            };
+        
+            request.Headers.Authorization = new AuthenticationHeaderValue("bearer", _accessToken);
+
+            return request;
+        }
+
+        public void RefreshToken(string token)
+        {
+            _accessToken = token;
+        }
+
+    }
+}
