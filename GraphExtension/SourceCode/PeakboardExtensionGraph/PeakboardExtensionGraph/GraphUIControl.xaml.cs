19aa976f03a97fc978904acfed97ebb941e961dd
Havano-ano <yannishartmann@onlinehome.de>
2023-03-14 16:21:15 +0100

    Added xaml file

---
+++ GraphExtension/SourceCode/PeakboardExtensionGraph/PeakboardExtensionGraph/GraphUIControl.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+//using System.Windows.Controls;
+using Peakboard.ExtensionKit;
+
+namespace PeakboardExtensionGraph
+{
+    public class GraphUIControl_xaml
+    {
+        
+    }
+}
