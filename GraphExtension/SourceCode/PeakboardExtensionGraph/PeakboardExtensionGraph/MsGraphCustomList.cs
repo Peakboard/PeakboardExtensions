a588afc778a67a8fbafbedbf1c48f8d11365920b
Havano-ano <yannishartmann@onlinehome.de>
2023-03-15 16:20:45 +0100

    json parsing
    GetColumnsOverride & GetItemsOverride now get values directly by parsing the json response

--- GraphExtension/SourceCode/PeakboardExtensionGraph/PeakboardExtensionGraph/MsGraphCustomList.cs
+++ GraphExtension/SourceCode/PeakboardExtensionGraph/PeakboardExtensionGraph/MsGraphCustomList.cs
@@ -2,6 +2,7 @@
 using System.Collections.Specialized;
 using System.Diagnostics;
 using System.IO;
+using System.Reflection;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Peakboard.ExtensionKit;
@@ -35,41 +36,86 @@ namespace PeakboardExtensionGraph
             { 
                 InitializeGraph(data);
             }
-
-            var cols = new CustomListColumnCollection
-            {
-                new CustomListColumn("id", CustomListColumnTypes.String),
-                new CustomListColumn("name", CustomListColumnTypes.String),
-                new CustomListColumn("email", CustomListColumnTypes.String)
-            };
             
+            data.Properties.TryGetValue("Data", StringComparison.OrdinalIgnoreCase, out var type);
+            var task = GraphHelper.MakeGraphCall(type);
+            task.Wait();
+            var response = task.Result;
+
+            var cols = new CustomListColumnCollection();
+            
+            JsonTextReader reader = new JsonTextReader(new StringReader(response));
+            bool start = false;
+            string lastValue = "";
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonToken.PropertyName) lastValue = reader.Value.ToString();
+                else if (reader.TokenType == JsonToken.StartArray && lastValue == "value") start = true;
+                else if (start && reader.TokenType == JsonToken.StartObject)
+                {
+                    ColumnsWalkThroughObject(reader, "root", cols);
+                    break;
+                }
+            }
+
+            if (!start)
+            {
+                reader = new JsonTextReader(new StringReader(response));
+                while (reader.Read())
+                {
+                    if (reader.TokenType == JsonToken.StartObject)
+                    {
+                        ColumnsWalkThroughObject(reader, "root", cols);
+                        break;
+                    }
+                }
+            }
+
             return cols;
         }
+        
 
         protected override CustomListObjectElementCollection GetItemsOverride(CustomListData data)
         {
-            data.Properties.TryGetValue("Data", StringComparison.OrdinalIgnoreCase, out var type);
-            
             if (!_initialized)
             { 
                 InitializeGraph(data);
             }
 
+            data.Properties.TryGetValue("Data", StringComparison.OrdinalIgnoreCase, out var type);
             var task = GraphHelper.MakeGraphCall(type);
             task.Wait();
-            
-            var contacts = JsonConvert.DeserializeObject<ContactList>(task.Result);
-            
+            var response = task.Result;
+
             var items = new CustomListObjectElementCollection();
 
-            foreach (var contact in contacts.value)
+            JsonTextReader reader = new JsonTextReader(new StringReader(response));
+            bool start = false;
+            string lastValue = "";
+            while (reader.Read())
             {
-                var newItem = new CustomListObjectElement();
-                newItem.Add("id", contact.id);
-                newItem.Add("name", contact.displayName);
-                newItem.Add("email", contact.emailAddresses[0].address);
-                
-                items.Add(newItem);
+                if (reader.TokenType == JsonToken.PropertyName) lastValue = reader.Value.ToString();
+                else if (reader.TokenType == JsonToken.StartArray && lastValue == "value") start = true;
+                else if (start && reader.TokenType == JsonToken.StartObject)
+                {
+                    var item = new CustomListObjectElement();
+                    ItemsWalkThroughObject(reader, "root", item);
+                    items.Add(item);
+                }
+            }
+            
+            if (!start)
+            {
+                reader = new JsonTextReader(new StringReader(response));
+                while (reader.Read())
+                {
+                    if (reader.TokenType == JsonToken.StartObject)
+                    {
+                        var item = new CustomListObjectElement();
+                        ItemsWalkThroughObject(reader, "root", item);
+                        items.Add(item);
+                    }
+                }
             }
 
             return items;
@@ -107,6 +153,150 @@ namespace PeakboardExtensionGraph
             }
             _initialized = true;
         }
+        
+        private void ColumnsWalkThroughObject(JsonReader reader, string objPrefix, CustomListColumnCollection cols)
+        {
+            var lastName = "";
+            var value = false;
+        
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonToken.PropertyName)
+                {
+                    lastName = reader.Value.ToString();
+                    value = true;
+                }
+                else if (reader.TokenType == JsonToken.StartObject)
+                {
+                    value = false;
+                    ColumnsWalkThroughObject(reader, $"{objPrefix}-{lastName}", cols);
+                }
+                else if (reader.TokenType == JsonToken.StartArray)
+                {
+                    cols.Add(new CustomListColumn($"{objPrefix}-{lastName}-Array", CustomListColumnTypes.String));
+                    SkipArray(reader);
+                    value = false;
+                }
+                else if (reader.TokenType == JsonToken.EndObject)
+                {
+                    return;
+                }
+                else if(value)
+                {
+                    Console.WriteLine($"{reader.TokenType} {objPrefix}-{lastName} = {reader.Value ?? "null"}"); 
+                    CustomListColumn newCol;
+                    if (reader.TokenType == JsonToken.Boolean)
+                    {
+                        newCol = new CustomListColumn($"{objPrefix}-{lastName}", CustomListColumnTypes.Boolean);
+                    }
+                    else if (reader.TokenType == JsonToken.Integer || reader.TokenType == JsonToken.Float)
+                    {
+                        newCol = new CustomListColumn($"{objPrefix}-{lastName}", CustomListColumnTypes.Number);
+                    }
+                    else
+                    {
+                        newCol = new CustomListColumn($"{objPrefix}-{lastName}", CustomListColumnTypes.String);
+                    }
+                    cols.Add(newCol);
+                    value = false;
+                }
+            }
+        }
+        
+        private void ItemsWalkThroughObject(JsonReader reader, string objPrefix, CustomListObjectElement item)
+        {
+            var lastName = "";
+            var value = false;
+        
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonToken.PropertyName)
+                {
+                    lastName = reader.Value.ToString();
+                    value = true;
+                }
+                else if (reader.TokenType == JsonToken.StartObject)
+                {
+                    value = false;
+                    ItemsWalkThroughObject(reader, $"{objPrefix}-{lastName}", item);
+                }
+                else if (reader.TokenType == JsonToken.StartArray)
+                {
+                    var arr = WalkThroughArray(reader, $"{objPrefix}-{lastName}-Array", item);
+                    item.Add($"{objPrefix}-{lastName}-Array", $"'{lastName}': [ {arr} ]");
+                }
+                else if (reader.TokenType == JsonToken.EndObject)
+                {
+                    return;
+                }
+                else if(value)
+                {
+                    if (reader.TokenType == JsonToken.Boolean || reader.TokenType == JsonToken.Float ||
+                        reader.TokenType == JsonToken.Integer)
+                    {
+                           item.Add($"{objPrefix}-{lastName}", reader.Value);
+                    }
+                    else
+                    {
+                        string objectValue = reader.Value?.ToString();
+                        if (objectValue?.Length - 1024 > 0)
+                        { 
+                            objectValue.Remove(1024);
+                        }
+                        item.Add($"{objPrefix}-{lastName}", objectValue);
+                    }
+                }
+            }
+        }
+        
+        private string WalkThroughArray(JsonReader reader, string objPrefix, CustomListObjectElement item)
+        {
+            bool value = false;
+            string lastname = "";
+            string arr = "";
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonToken.PropertyName)
+                {
+                    value = true;
+                    lastname = reader.Value.ToString();
+                }
+                else if (reader.Value != null && value)
+                {
+                    arr += $"'{lastname}': '{reader.Value}', ";
+                }
+                else if (reader.TokenType == JsonToken.StartArray)
+                {
+                    WalkThroughArray(reader, objPrefix, item);
+                }
+                else if (reader.TokenType == JsonToken.EndArray)
+                {
+                    if (arr.Length > 2)
+                    {
+                        arr.Remove(arr.Length - 2);
+                    }
+                    return arr;
+                }
+            }
+
+            return arr;
+        }
+
+        private void SkipArray(JsonReader reader)
+        {
+            while (reader.Read())
+            {
+                if (reader.TokenType == JsonToken.StartArray)
+                {
+                    SkipArray(reader);
+                }
+                else if (reader.TokenType == JsonToken.EndArray)
+                {
+                    return;
+                }
+            }
+        }
+
         
     }
 }
