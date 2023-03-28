ff52fde851e6d17b2d7839de1d50575e1acea244
Havano-ano <yannishartmann@onlinehome.de>
2023-03-28 14:56:43 +0200

    Added Custom List Function

--- GraphExtension/SourceCode/GraphFunctionTestConsole/ConsoleApplication1/Program.cs
+++ GraphExtension/SourceCode/GraphFunctionTestConsole/ConsoleApplication1/Program.cs
@@ -1,4 +1,5 @@
-﻿using PeakboardExtensionGraph;
+﻿using System;
+using PeakboardExtensionGraph;
 
 namespace ConsoleApplication1
 {
@@ -6,7 +7,18 @@ namespace ConsoleApplication1
     {
         public static void Main(string[] args)
         {
-            
+            var template =
+                "(\"message\":(\"subject\":\"{0}\",\"body\":(\"contentType\":null,\"content\":\"{1}\"),\"toRecipients\":[(\"emailAddress\":(\"name\":null,\"address\":\"{2}\"))]))";
+
+            var recipient = "yh@email.com";
+            var header = "test";
+            var body = "body";
+
+            var requestBody = String.Format(template, header, body, recipient);
+            requestBody = requestBody.Replace('(', '{');
+            requestBody = requestBody.Replace(')', '}');
+
+            Console.WriteLine(requestBody);
         }
     }
 }
