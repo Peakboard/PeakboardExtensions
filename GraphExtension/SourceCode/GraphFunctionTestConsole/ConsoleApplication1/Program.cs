f0b14442d3f03718462f3d36ee1ed8b0ceec22e8
Yannis <92931646+yhartm@users.noreply.github.com>
2023-04-06 15:14:08 +0200

    Added FunctionsCustomList

--- GraphExtension/SourceCode/GraphFunctionTestConsole/ConsoleApplication1/Program.cs
+++ GraphExtension/SourceCode/GraphFunctionTestConsole/ConsoleApplication1/Program.cs
@@ -13,7 +13,7 @@ namespace ConsoleApplication1
     {
         public static void Main(string[] args)
         {
-            var helper = new GraphHelperAppOnly("067207ed-41a4-4402-b97f-b977babe0ec9",
+            /*var helper = new GraphHelperAppOnly("067207ed-41a4-4402-b97f-b977babe0ec9",
                 "b4ff9807-402f-42b8-a89d-428363c55de7", "4Sa8Q~kl8UcvQPUrLrMkVudIeIb6XHJ4l8K95cr6");
 
             helper.InitGraph().Wait();
@@ -41,8 +41,20 @@ namespace ConsoleApplication1
                 {
                     Console.WriteLine($"Token: {reader.TokenType}");
                 }
-            }
+            }*/
 
+            long time1 = DateTimeOffset.Now.ToUnixTimeSeconds();
+            string str =
+                "{\r\n    \"message\": {\r\n        \"subject\": \"$0$\",\r\n        \"body\": {\r\n            \"contentType\": \"Text\",\r\n            \"content\": \"$1$\"\r\n        },\r\n        \"toRecipients\": [\r\n            {\r\n                \"emailAddress\": {\r\n                    \"address\": \"$2$\"\r\n                }\r\n            }\r\n        ]\r\n    }\r\n}|{\r\n    \"title\": \"$0$\"\r\n}";
+            string[] arr = str.Split('|');
+            long time2 = DateTimeOffset.Now.ToUnixTimeSeconds();
+            
+            
+            Console.WriteLine(arr[0]);
+            Console.WriteLine();
+            Console.WriteLine(arr[1]);
+            Console.WriteLine(time2-time1);
+            
         }
     }
 }
