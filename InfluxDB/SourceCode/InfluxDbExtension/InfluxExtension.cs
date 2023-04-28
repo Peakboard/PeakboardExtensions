b46016fbf05661e762cf464b9bb855d7e812408d
Yannis <92931646+yhartm@users.noreply.github.com>
2023-04-28 16:19:28 +0200

    Added InfluxDb Extension

---
+++ InfluxDB/SourceCode/InfluxDbExtension/InfluxExtension.cs
@@ -0,0 +1,29 @@
+﻿using Peakboard.ExtensionKit;
+
+namespace InfluxDbExtension
+{
+    
+    [ExtensionIcon("InfluxDbExtension.pb_datasource_influx.png")]
+    public class InfluxExtension : ExtensionBase
+    {
+        protected override ExtensionDefinition GetDefinitionOverride()
+        {
+            return new ExtensionDefinition()
+            {
+                ID = "InfluxDbExtension",
+                Name = "InfluxDB Extension",
+                Author = "Yannis Hartmann",
+                Version = "1.0",
+            };
+        }
+
+        protected override CustomListCollection GetCustomListsOverride()
+        {
+            return new CustomListCollection()
+            {
+                new InfluxDbQueryCustomList(),
+                new InfluxDbWriteCustomList()
+            };
+        }
+    }
+}
