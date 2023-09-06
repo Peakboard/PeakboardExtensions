3e8ce2b43b3e65cae74719392ab7dfe1f37e4da1
Thilo Brosinsky <thilo.brosinsky@peakboard.com>
2023-09-06 14:29:00 +0200

    Added databricks extension

---
+++ Databricks/SourceCode/DatabricksDataSource.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Peakboard.ExtensionKit;
+
+
+namespace Databricks
+{
+    [ExtensionIcon("Databricks.databricks.png")]
+    public class DatabricksExtension : ExtensionBase
+    {
+        protected override ExtensionDefinition GetDefinitionOverride()
+        {
+            return new ExtensionDefinition
+            {
+                ID = "Databricks",
+                Name = "Databricks Extension",
+                Description = "Get your data from Databricks",
+                Version = "1.0",
+                Author = "Thilo Brosinsky",
+                Company = "Peakboard GmbH.",
+                Copyright = "Copyright © Peakboard GmbH",
+            };
+        }
+
+        protected override CustomListCollection GetCustomListsOverride()
+        {
+            return new CustomListCollection
+            {
+                new DatabricksCustomList(),
+            };
+        }
+    }
+}
