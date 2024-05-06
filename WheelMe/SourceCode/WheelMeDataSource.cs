1b34ccdd1929d1d1979d4cfc083b664b434ff7aa
Ruslan Kachkovskyi <ruslan@wheel.me>
2024-05-07 01:24:42 +0200

    Cleanup usings

--- WheelMe/SourceCode/WheelMeDataSource.cs
+++ WheelMe/SourceCode/WheelMeDataSource.cs
@@ -2,7 +2,6 @@
 using Newtonsoft.Json.Linq;
 using System.Net.Http;
 using Peakboard.ExtensionKit;
-using System.Net.NetworkInformation;
 
 namespace WheelMe
 {
--- WheelMe/SourceCode/WheelMeFloorsCustomList.cs
+++ WheelMe/SourceCode/WheelMeFloorsCustomList.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Net.Http;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
-using System.Collections.Generic;
 using Peakboard.ExtensionKit;
 using Newtonsoft.Json.Linq;
 
