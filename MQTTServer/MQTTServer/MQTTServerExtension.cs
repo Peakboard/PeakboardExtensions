c3ad864b4ae86980f1aa25b1f0ad9143cee5fd6c
Thilo Brosinsky <thilo.brosinsky@peakboard.com>
2025-01-08 17:03:45 +0100

    MQTT Server first version - not fully working

---
+++ MQTTServer/MQTTServer/MQTTServerExtension.cs
@@ -0,0 +1,26 @@
+﻿using Peakboard.ExtensionKit;
+
+namespace MQTTServer
+{
+    public class MQTTServerExtension : ExtensionBase
+    {
+        protected override ExtensionDefinition GetDefinitionOverride()
+        {
+            return new ExtensionDefinition
+            {
+                ID = "MQTTServer",
+                Name = "MQTTServer",
+                Description = "This extension provides information about MQTTServer.",
+                Version = "1.0",
+            };
+        }
+
+        protected override CustomListCollection GetCustomListsOverride()
+        {
+            return new CustomListCollection
+            {
+                new MQTTServerList()
+            };
+        }
+    }
+}
