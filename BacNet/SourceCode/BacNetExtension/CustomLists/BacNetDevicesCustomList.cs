22728f87743b01dd848e1c787eca753b761af652
Makhsum <dd4006189@gmail.com>
2025-02-27 13:02:04 +0100

    fixed DeviceCustomList Bug

--- BacNet/SourceCode/BacNetExtension/CustomLists/BacNetDevicesCustomList.cs
+++ BacNet/SourceCode/BacNetExtension/CustomLists/BacNetDevicesCustomList.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.IO.BACnet;
 using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace BacNetExtension.CustomLists
 {
@@ -14,7 +15,9 @@ namespace BacNetExtension.CustomLists
     {
         private BacnetClient _client;
         private List<Device> _devices;
-        private ManualResetEventSlim _waitHandle;
+        private DateTime _lastEventTime;
+        private const int TimeoutMilliseconds = 2000;
+
         protected override CustomListDefinition GetDefinitionOverride()
         {
             return new CustomListDefinition
@@ -44,36 +47,79 @@ namespace BacNetExtension.CustomLists
 
         protected override CustomListObjectElementCollection GetItemsOverride(CustomListData data)
         {
-            var tcpPort = int.Parse(data.Properties["Port"]);
-            var objectElementCollection = new CustomListObjectElementCollection();
-            StartConnection(tcpPort);
-
-            foreach (var device in _devices)
+            var task = Task.Run(async () =>
             {
-                var itemElement = new CustomListObjectElement()
+                try
                 {
-                    { "Address", device.Address.ToString() },
-                    { "DeviceID", device.DeviceId },
-                    { "MaxAdpu", device.MaxAdpu },
-                    { "Segmentation", device.Segmentation.ToString() },
-                    { "VendorID", device.VendorId.ToString() }
-                };
-                objectElementCollection.Add(itemElement);
-            }
+                    if (int.TryParse(data.Properties["Port"], out int tcpPort))
+                    {
+                        var objectElementCollection = new CustomListObjectElementCollection();
 
-            return objectElementCollection;
+                        try
+                        {
+                            await StartConnection(tcpPort);
+                        }
+                        catch (Exception ex)
+                        {
+                            Log.Error($"Error while starting connection: {ex}");
+                            return objectElementCollection;
+                        }
+
+                        if (_devices != null && _devices.Count > 0)
+                        {
+                            foreach (var device in _devices)
+                            {
+                                Log.Info($"Device found: Address={device.Address}, DeviceID={device.DeviceId}, VendorID={device.VendorId}");
+                                var itemElement = new CustomListObjectElement()
+                                {
+                                    { "Address", device.Address.ToString() },
+                                    { "DeviceID", device.DeviceId },
+                                    { "MaxAdpu", device.MaxAdpu },
+                                    { "Segmentation", device.Segmentation.ToString() },
+                                    { "VendorID", device.VendorId.ToString() }
+                                };
+                                objectElementCollection.Add(itemElement);
+                            }
+                        }
+                        else
+                        {
+                            Log.Info("No devices found.");
+                        }
+
+                        return objectElementCollection;
+                    }
+                    else
+                    {
+                        throw new ArgumentException(
+                            "Invalid port number. Please ensure that the port is a valid integer.");
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw;
+                }
+            });
+
+            task.Wait();
+            return task.Result;
         }
 
-        private void StartConnection(int port)
+        private async Task StartConnection(int port)
         {
-            _waitHandle = new ManualResetEventSlim(false);
             _devices = new List<Device>();
             var transport = new BacnetIpUdpProtocolTransport(port);
             _client = new BacnetClient(transport);
             _client.Start();
             _client.OnIam += OnIamReceived;
             _client.WhoIs();
-            _waitHandle.Wait(3000);
+
+            _lastEventTime = DateTime.Now;
+            Log.Info("Waiting for devices...");
+
+            while ((DateTime.Now - _lastEventTime).TotalMilliseconds < TimeoutMilliseconds)
+            {
+                await Task.Delay(100);
+            }
         }
 
         private void OnIamReceived(BacnetClient sender, BacnetAddress adr, uint deviceId, uint maxApdu, BacnetSegmentations segmentation, ushort vendorId)
@@ -90,9 +136,10 @@ namespace BacNetExtension.CustomLists
             if (_devices.FirstOrDefault(d => d.DeviceId == device.DeviceId) == null)
             {
                 _devices.Add(device);
-                _waitHandle.Set();
+                Log.Info($"Device added: DeviceID={device.DeviceId}, Address={device.Address}");
             }
-            
+
+            _lastEventTime = DateTime.Now;
         }
     }
 }
