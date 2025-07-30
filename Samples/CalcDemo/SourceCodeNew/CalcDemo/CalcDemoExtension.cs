9fa17bcff267f9beefab01f463fed1c3578e3a13
Jürgen Bäurle <jbaeurle@parago.de>
2025-07-30 11:38:51 +0200

    Added new .NET8 extension (CalcDemo)

---
+++ Samples/CalcDemo/SourceCodeNew/CalcDemo/CalcDemoExtension.cs
@@ -0,0 +1,28 @@
+﻿using Peakboard.ExtensionKit;
+
+namespace CalcDemo;
+
+public class CalcDemoExtension : ExtensionBase
+{
+    protected override ExtensionDefinition GetDefinitionOverride()
+    {
+        return new ExtensionDefinition
+        {
+            ID = "CalcDemo",
+            Name = "Calculation demo",
+            Description = "This is a sample implementation for input and output parameters",
+            Version = "1.0",
+            Author = "Thilo Brosinsky",
+            Company = "Peakboard GmbH",
+            Copyright = "Peakboard GmbH",
+        };
+    }
+
+    protected override CustomListCollection GetCustomListsOverride()
+    {
+        return
+        [
+            new CalcDemoCustomList(),
+        ];
+    }
+}
