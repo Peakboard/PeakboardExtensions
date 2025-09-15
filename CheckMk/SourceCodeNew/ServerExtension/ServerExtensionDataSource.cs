76e3726145ec10173e0f0001449041039b5a4ef2
Shukrullo Makhsum Yusupov <dd4006189@gmail.com>
2025-09-15 16:44:33 +0200

    updated CheckMkExtension to .net8

---
+++ CheckMk/SourceCodeNew/ServerExtension/ServerExtensionDataSource.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Peakboard.ExtensionKit;
+namespace CheckMkExtension
+{
+    [ExtensionIcon("ServerExtension.icon.png")]
+    internal class CheckMkExtension : ExtensionBase
+    {
+        protected override ExtensionDefinition GetDefinitionOverride()
+        {
+            return new ExtensionDefinition
+            {
+                ID = "CheckMkExtension",
+                Name = "CheckMk",
+                Description = "Interface with the Server Service and Host Problems",
+                Version = "1.0",
+                Author = "Makhsum",
+                Company = "Peakboard",
+                Copyright = "Your Copyright"
+            };            
+        }
+        protected override CustomListCollection GetCustomListsOverride()
+        {
+            return new CustomListCollection
+            {
+                new CheckMkExtensionCustomList(),
+            };
+        }
+    }
+}
