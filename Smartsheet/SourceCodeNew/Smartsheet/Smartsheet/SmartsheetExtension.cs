084690a526cafc5821c56a6b4c8ab9a8840f4f92
Shukrullo Makhsum Yusupov <dd4006189@gmail.com>
2025-09-16 15:16:39 +0200

    added new .net8 Extension (Smartsheet)

---
+++ Smartsheet/SourceCodeNew/Smartsheet/Smartsheet/SmartsheetExtension.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using Peakboard.ExtensionKit;
+
+namespace Smartsheet
+{
+    [ExtensionIcon("Smartsheet.Smartsheet.png")]
+    public class SmartsheetExtension : ExtensionBase
+    {
+        public SmartsheetExtension(IExtensionHost host) : base(host) 
+        { 
+        
+        }
+        
+        protected override ExtensionDefinition GetDefinitionOverride()
+        {
+            return new ExtensionDefinition
+            {
+                ID = "Smartsheet",
+                Name = "Smartsheet",
+                Description = "Get your data from Smartsheet",
+                Version = "1.0",
+                Author = "Michelle Wu",
+                Company = "Peakboard GmbH.",
+                Copyright = "Copyright © Peakboard GmbH",
+            };
+        }
+
+        protected override CustomListCollection GetCustomListsOverride()
+        {
+            return new CustomListCollection
+            {
+                new SmartsheetTableListCustomList(),
+                new SmartsheetTableDataCustomList(),
+            };
+        }
+
+        private static string MyLocalToken = string.Empty;
+
+
+    }
+}
---
+++ Smartsheet/SourceCodeNew/Smartsheet/Smartsheet/SmartsheetTableListCustomList.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using Peakboard.ExtensionKit;
+using Newtonsoft.Json.Linq;
+using System.Text;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace Smartsheet
+{
+    [Serializable]
+    [CustomListIcon("Smartsheet.Smartsheet.png")]
+    
+    class SmartsheetTableListCustomList : CustomListBase
+    {
+        protected override CustomListDefinition GetDefinitionOverride()
+        {
+            return new CustomListDefinition
+            {
+                ID = "SmartsheetTableList",
+                Name = "SmartsheetTableList",
+                Description = "Fetches data from Smartsheet API",
+                PropertyInputPossible = true,
+                PropertyInputDefaults = {
+                new CustomListPropertyDefinition() { Name = "Token", Value = "XXX" },
+
+                    },
+                Functions = new CustomListFunctionDefinitionCollection
+                {
+                    
+                }     
+            };
+        }
+
+        protected override void CheckDataOverride(CustomListData data)
+        {
+            if (string.IsNullOrWhiteSpace(data.Properties["Token"]))
+            {
+                throw new InvalidOperationException("Invalid Token");
+            }
+        }
+
+        protected override CustomListColumnCollection GetColumnsOverride(CustomListData data)
+        {
+            var columns = new CustomListColumnCollection();
+            columns.Add(new CustomListColumn("ID", CustomListColumnTypes.String));
+            columns.Add(new CustomListColumn("Name", CustomListColumnTypes.String));
+            return columns;
+        }
+
+        protected override CustomListObjectElementCollection GetItemsOverride(CustomListData data)
+        {
+            var items = new CustomListObjectElementCollection();
+
+            using (HttpClient client = new HttpClient())
+            {
+
+                client.DefaultRequestHeaders.Add("Authorization", "Bearer " + data.Properties["Token"]);
+
+                HttpResponseMessage response = client.GetAsync("https://api.smartsheet.com/2.0/sheets").Result;
+                var responseBody = response.Content.ReadAsStringAsync().Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var dyn = JsonConvert.DeserializeObject<dynamic>(responseBody);
+                    foreach (var row in dyn.data)
+                    {
+                        var item = new CustomListObjectElement();
+                        item.Add("ID", Convert.ToString( row["id"]));
+                        item.Add("Name", Convert.ToString(row["name"]));
+                        items.Add(item);
+                    }
+                }
+                else
+                {
+                    throw new Exception( "Error during call -> " + response.StatusCode + response.ReasonPhrase);
+                }
+
+            }
+            return items;
+        }
+
+    }
+}
