082dbf741f84432d5022cc57efec6b7e5799d773
Shukrullo Makhsum Yusupov <dd4006189@gmail.com>
2025-09-16 10:17:08 +0200

    added new .net8 Extension Ingris

---
+++ Ingres/SourceCodeNew/PeakboardExtensionIngres/IngresExtension.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Peakboard.ExtensionKit;
+
+
+namespace PeakboardExtensionIngres
+{
+    public class IngresExtension : ExtensionBase
+    {
+        public IngresExtension(IExtensionHost host) : base(host)
+        {
+        }
+
+
+        protected override ExtensionDefinition GetDefinitionOverride()
+        {
+            return new ExtensionDefinition
+            {
+                ID = "Ingres", // Must be unqiue over all extensions, so may use a namespace notation
+                Name = "Ingres Extension",
+                Description = "This is an Extension for accessing Ingres data",
+                Version = "1.0",
+                Author = "Peakboard Team",
+                Company = "Peakboard GmbH",
+                Copyright = "Copyright © 2020",
+            };
+        }
+
+        protected override CustomListCollection GetCustomListsOverride()
+        {
+            return new CustomListCollection
+            {
+                new IngresCustomList(),
+            };
+        }
+    }
+}
